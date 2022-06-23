5de5bd384340be798924840f6c3f45637a67f090
jbc0503 <jbc0503@lgcns.com>
2022-06-23 18:37:39 +0900

    rundemo

---
+++ src/main/java/com/example/demo/RunDemo.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class RunDemo {
+	public static void main(String[] args) {
+		System.out.println("Server running..");
+		//SpringApplication.run(DemoApplication.class, args);
+	}
+}
